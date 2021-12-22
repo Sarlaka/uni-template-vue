2c591b395ce968a675409c33b7e2aa88e0cc4467
duchengdong <xinyiyake006@163.com>
2021-12-22 20:12:08 +0800

    添加uview使用

--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@
  * @Author: duchengdong
  * @Date: 2021-12-21 16:39:07
  * @LastEditors: duchengdong
- * @LastEditTime: 2021-12-21 17:42:04
+ * @LastEditTime: 2021-12-22 20:11:42
  * @Description: 
  */
 import Vue from 'vue'
 import App from './App'
 import * as config from 'config/config'
 import store from './store'
+import uView from 'uview-ui'
+
+// 配置uview
+Vue.use(uView)
+// 如此配置即可
+uni.$u.config.unit = 'rpx'
 
 Vue.prototype.$config = config
 Vue.prototype.$store = store
